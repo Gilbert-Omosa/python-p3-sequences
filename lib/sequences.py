5d395a887cfc1362315e4e9a4b504218f474679c
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-11-24 15:38:37 +0300

    Done with Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        fib_sequence = []  # Initialize an empty sequence for length 0
+    elif length == 1:
+        fib_sequence = [0]  # Initialize sequence with 0 for length 1
+    else:
+        fib_sequence = [0, 1]  # Initialize sequence with 0 and 1
+        while len(fib_sequence) < length:
+            next_number = fib_sequence[-1] + fib_sequence[-2]  # Calculate the next number in the sequence
+            fib_sequence.append(next_number)  # Add the next number to the sequence
+    print(fib_sequence)  # Print the Fibonacci sequence
